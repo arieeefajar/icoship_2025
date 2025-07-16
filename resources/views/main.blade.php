5b18052b768f77ed5d51a461091276f33ce6b9bd
arieeefajar <ariefajar739@gmail.com>
2025-07-16 09:59:37 +0700

    feat(landing): link submit in hero section

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -18,16 +18,16 @@
                             <p>{{ $config['conference']->theme }}</p>
                         </div>
 
-                        <div class="d-flex flex-column flex-md-row gap-3">
+                        <div class="d-flex flex-column flex-md-row gap-2">
                             <div class="cta-button">
-                                <a href="#services" class="btn">
+                                <a href="{{ $config['conference']->introduction_link }}" class="btn">
                                     <span>Introduction</span>
                                     <i class="bi bi-info-circle"></i>
                                 </a>
                             </div>
 
                             <div class="cta-button">
-                                <a href="#services" class="btn">
+                                <a href="{{ $config['conference']->register_link }}" target="_blank" class="btn">
                                     <span>Submit Here</span>
                                     <i class="bi bi-upload"></i>
                                 </a>
@@ -35,7 +35,7 @@
 
                             <div class="cta-button">
                                 <a href="#services" class="btn">
-                                    <span>Poster ICoFA 2025</span>
+                                    <span>Poster ICoSHIP 2025</span>
                                     <i class="bi bi-file-earmark-post"></i>
                                 </a>
                             </div>
@@ -62,7 +62,7 @@
     </section><!-- /Hero Section -->
 
     <!-- About Section -->
-    <section id="about" class="about section">
+    <section id="introduction" class="about section">
 
         <!-- Section Title -->
         <div class="container section-title" data-aos="fade-up">
